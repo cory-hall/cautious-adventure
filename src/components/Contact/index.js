170de8a8b83f966816f85c43473f6710d9604615
Cory Hall <ComebackCory1@gmail.com>
2022-06-24 18:23:37 -0500

    enabled emailjs

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { validateEmail } from '../../utils/helpers';
+import emailjs from '@emailjs/browser';
 
 function ContactForm() {
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-  const { name, email, message } = formState;
+  var { name, email, message } = formState;
   const [errorMessage, setErrorMessage] = useState('');
+
+  const form = useRef();
 
   function handleChange(e) {
     if (e.target.name === 'email') {
@@ -27,32 +30,42 @@ function ContactForm() {
     }
   };
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    emailjs.send("service_5ou4udn","template_yapyoo6", {email, name, message}, 'Bl-To2-JWLoD6rxHN')
+      .then((result) => {
+        console.log(result);
+        setErrorMessage('Message successfully sent!');
+        setFormState({ name: '', email: '', message: '' });
+      }, (error) => {
+        console.log(error);
+        setErrorMessage('Something went wrong. Try sending me a direct email at corycareerwebdev@gmail.com')
+      });
   }
 
   return (
     <section className='bg-color radius w-5/6 md:w-3/5 p-10'>
       <h1 className='text-3xl text-center'>Contact me</h1>
-      <form className='flex flex-col items-center md:items-end md:w-2/3 m-4'>
+      <form ref={form} onSubmit={handleSubmit} className='flex flex-col items-center md:items-end md:w-2/3 m-4'>
         <div className='mobile-form py-4'>
           <label className='px-2' htmlFor="name">Name:</label>
-          <input className='px-4' type="text" defaultValue={name} onBlur={handleChange} name="name" />
+          <input className='px-4' type="text" defaultValue={name} onChange={handleChange} name="name" required />
         </div>
         <div className='mobile-form py-4'>
           <label className='px-2' htmlFor="email">Email address:</label>
-          <input className='px-4' type="email" defaultValue={email} onBlur={handleChange} name="email" />
+          <input className='px-4' type="email" defaultValue={email} onChange={handleChange} name="email" required/>
         </div>
         <div className='flex flex-wrap  mobile-form content-center py-4'>
           <label className='px-2' htmlFor="message">Message:</label>
-          <textarea className='px-5' name="message" defaultValue={message} onBlur={handleChange} rows="5" />
+          <textarea className='px-5' name="message" defaultValue={message} onChange={handleChange} rows="5" required/>
         </div>
         {errorMessage && (
           <div className='m-3'>
             <p className='error-text'>{errorMessage}</p>
           </div>
         )}
-        <button className='bg-white p-1 px-2' type="submit" onSubmit={handleSubmit}>Submit</button>
+        <button className='bg-white p-1 px-2' type="submit">Submit</button>
       </form>
     </section>
   );
