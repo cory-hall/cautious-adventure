7499850cc5fe549ad461e395db2ab47f66f56ab2
Cory Hall <ComebackCory1@gmail.com>
2022-06-26 18:24:50 -0500

    updated picture, fixed resume

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-import portpic from '../../assets/images/port-pic.jpg';
+import portpic from '../../assets/images/port-pic.JPG';
 
 function About() {
   return (
-    <section className="flex flex-wrap items-center p-10 w-5/6 bg-color radius" >
+    <section className="flex flex-wrap justify-around items-center p-10 w-5/6 bg-color radius" >
       <h1 className="text-3xl text-center text-white font-bold pb-4 w-full">Who am I?</h1>
-      <div className="md:w-1/3">
-        <img className='rounded-full block mx-auto' src={portpic} alt='Cory Hall'></img>
+      <div className="md:w-1/3 mr-4">
+        <img className='rounded-2xl block mx-auto' src={portpic} alt='Cory Hall'></img>
       </div>
-      <div className="md:w-2/3">
+      <div className="md:w-1/2">
         <p className="p-2 text-xl">
           I am an aspiring web developer, who through deliberate practice and dedication am working towards becoming a full fledged web developer.
           I am a recent recipient of a Full Stack Web Development certificate from a coding boot camp hosted by Michigan State University.
